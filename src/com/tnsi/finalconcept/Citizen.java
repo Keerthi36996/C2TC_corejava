c4dc1f642f361c2bcb5fe664f5b3606f8cc92624
Keerthi M <keerthi.m4143@gmail.com>
2026-05-01 19:28:16 +0530

    Initial Commit

---
+++ src/com/tnsi/finalconcept/Citizen.java
@@ -0,0 +1,22 @@
+package com.tnsi.finalconcept;
+   public class Citizen {
+		String name;
+		final static String countryName="India";
+		
+		public  Citizen(String name) {
+			this.name = name;
+		}
+
+		void m1() {
+			System.out.println("In method 1");
+		}
+		
+		final void m2() {
+			System.out.println("In method 2");
+		}
+
+		@Override
+		public String toString() {
+			return "Citizen [name =" +" "+ name +" " +","+"country ="+" "+countryName +"]";
+		}
+}
---
+++ src/com/tnsi/finalconcept/FinalDemo.java
@@ -0,0 +1,13 @@
+package com.tnsi.finalconcept;
+public class FinalDemo {
+
+	public static void main(String[] args) {
+		Citizen c1=new Citizen("Ravi");
+		Citizen c2=new Citizen("Raju");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+
+	}
+
+}
